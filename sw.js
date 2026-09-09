f95cf139aa2b480322c0bfae792cae69248de1d7
shibayamakazuki0520 <shibayamakazuki0520@gmail.com>
2026-09-09 23:30:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "umehara-progress-v2";
+const CACHE_NAME = "umehara-progress-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
